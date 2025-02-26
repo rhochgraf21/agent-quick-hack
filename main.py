c60931134dacce8d0b009b671db9b223519f0df1
AparCode <apar2003@gmail.com>
2025-02-25 19:26:43 -0500

    wrote the prompt input function

--- main.py
+++ main.py
@@ -2,34 +2,43 @@
 # ----
 
 import os
-import dotenv
+from dotenv import load_dotenv
+from pydantic import BaseModel
 from openai import OpenAI
 
-key = os.getenv("OPENAI_KEY")
+from langchain_core.messages import HumanMessage
 
-model  = OpenAI(model="gpt-4o-mini", api_key=os.environ["OPENAI_API_KEY"])
+load_dotenv()
+key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
+
+class Step(BaseModel):
+    explanation: str
+    output: str
+
+class MathReasoning(BaseModel):
+    steps: list[Step]
+    final_answer: str
+
+def generate_summary(input, prompt):
+    message = client.invoke([HumanMessage(
+        content=[{"type": "text", "text": prompt},]
+    )])
+    return message.content
 
 
-# from pydantic import BaseModel
-# from openai import OpenAI
 
-# client = OpenAI()
 
-# class Step(BaseModel):
-#     explanation: str
-#     output: str
 
-# class MathReasoning(BaseModel):
-#     steps: list[Step]
-#     final_answer: str
+i = input("write a prompt")
 
-# completion = client.beta.chat.completions.parse(
-#     model="gpt-4o-2024-08-06",
-#     messages=[
-#         {"role": "system", "content": "You are a helpful math tutor. Guide the user through the solution step by step."},
-#         {"role": "user", "content": "how can I solve 8x + 7 = -23"}
-#     ],
-#     response_format=MathReasoning,
-# )
+completion = client.beta.chat.completions.parse(
+    model="gpt-4o-2024-08-06",
+    messages=[
+        {"role": "developer", "content": [{"type":"text", "text":"You are in intelligent AI assistant specializing in converting natural language to a SQL query."}]},
+        {"role": "user", "content": [{"type":"text", "text": i}]}
+    ],
+    response_format=MathReasoning,
+)
 
-# math_reasoning = completion.choices[0].message.parsed
+math_reasoning = completion.choices[0].message.parsed
